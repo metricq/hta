7e829c91a7400de4a2cc78e1186f2b5d3cfa1e86
tilsche <thomas.ilsche@tu-dresden.de>
2018-05-18 19:38:36 +0200

    Better dump

--- src/tools/dump.cpp
+++ src/tools/dump.cpp
@@ -1,6 +1,8 @@
 #include "../storage/file/file.hpp"
 #include "../storage/file/metric.hpp"
 
+#include <hta/ostream.hpp>
+
 #include <iostream>
 
 #include <cassert>
@@ -9,24 +11,41 @@ namespace hta::storage::file
 {
 void dump_header(const std::filesystem::path& path)
 {
-    File<Metric::Header, TimePoint> file{ FileOpenTag::Read(), path };
-    auto header = file.header();
-    std::cout << "HTA header of " << path << "\n";
-    std::cout << "version: " << header.version << "\n";
-    std::cout << "interval: " << header.interval << "\n";
-    std::cout << "duration_period: " << header.duration_period.num << " / "
-              << header.duration_period.den << "\n";
-    std::cout << "interval_min: " << header.interval_min << "\n";
-    std::cout << "interval_factor: " << header.interval_factor << "\n";
-
-    if (file.size() > 0)
+    bool is_raw;
     {
-        std::cout << "first timestamp: " << file.read(0).time_since_epoch().count() << "\n";
-        std::cout << "last timestamp: " << file.read(file.size() - 1).time_since_epoch().count()
-                  << "\n";
+        File<Metric::Header, TimePoint> file{ FileOpenTag::Read(), path };
+        auto header = file.header();
+        std::cout << "HTA header of " << path << "\n";
+        std::cout << "version: " << header.version << "\n";
+        std::cout << "interval: " << header.interval << "\n";
+        std::cout << "duration_period: " << header.duration_period.num << " / "
+                  << header.duration_period.den << "\n";
+        std::cout << "interval_min: " << header.interval_min << "\n";
+        std::cout << "interval_factor: " << header.interval_factor << "\n";
+        is_raw = (header.interval == 0);
     }
-
-    std::cout << file.size() << " 8-byte values\n";
+    if (is_raw)
+    {
+        File<Metric::Header, TimeValue> file{ FileOpenTag::Read(), path };
+        if (file.size() > 0)
+        {
+            std::cout << "first timestamp: " << file.read(0).time << "\n";
+            std::cout << "last timestamp " << file.read(file.size() - 1).time << "\n";
+        }
+        std::cout << file.size() << " raw values\n";
+    }
+    else
+    {
+        {
+            File<Metric::Header, TimeAggregate> file{ FileOpenTag::Read(), path };
+            if (file.size() > 0)
+            {
+                std::cout << "first timestamp: " << file.read(0).time << "\n";
+                std::cout << "last timestamp " << file.read(file.size() - 1).time << "\n";
+            }
+            std::cout << file.size() << " aggregate values\n";
+        }
+    }
 }
 } // namespace hta::storage::file
 
