a5317d63ee5019b87b3e007dda120ae667f44286
Franz Schmidt <kinnarr@hollywuuud.de>
2018-09-07 16:30:51 +0200

    Adds license

--- src/storage/file/file.hpp
+++ src/storage/file/file.hpp
@@ -1,3 +1,32 @@
+// Copyright (c) 2018, ZIH,
+// Technische Universitaet Dresden,
+// Federal Republic of Germany
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of metricq nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include "../directory.hpp"
@@ -35,7 +64,7 @@ namespace FileOpenTag
     {
         static constexpr OpenMode mode = OpenMode::read_write;
     };
-}
+} // namespace FileOpenTag
 
 class Exception : public hta::Exception
 {
@@ -256,13 +285,11 @@ private:
         }
     }
 
-    [[noreturn]] void raise_stream_error(const std::string& message)
-    {
+    [[noreturn]] void raise_stream_error(const std::string& message) {
         throw Exception(message, filename_, errno);
     }
 
-private:
-    std::filesystem::path filename_;
+    private : std::filesystem::path filename_;
     std::fstream stream_;
     // Apparently pos_type doesn't like to be constexpr m(
     static constexpr size_type header_begin_ =
@@ -272,4 +299,4 @@ private:
     pos_type data_begin_;
     HeaderType header_;
 };
-}
+} // namespace hta::storage::file
